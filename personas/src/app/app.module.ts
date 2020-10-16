c7490f324aa44c84c6d74ae78c58ac239f290778
Anderson <fabiander_21@hotmail.es>
2020-10-16 03:48:36 -0500

    ruta de error

--- personas/src/app/app.module.ts
+++ personas/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { PersonasService } from './personas.service';
 import { FormularioComponent } from './personas/formulario/formulario.component';
 import { LogginService } from './loggin.service';
 import { PersonasComponent } from './personas/personas.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonaComponent,
     FormularioComponent,
-    PersonasComponent
+    PersonasComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
