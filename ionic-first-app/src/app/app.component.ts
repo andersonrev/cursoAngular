8f0a004ae691f21624974283b358c99042368758
Anderson <fabiander_21@hotmail.es>
2021-04-14 08:46:03 -0500

    importar menus en menu 1 forma con ngOnInit

--- ionic-first-app/src/app/app.component.ts
+++ ionic-first-app/src/app/app.component.ts
@@ -1,27 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {DataService} from './services/data.service';
+import {Observable} from 'rxjs';
+import {Componente} from './interfaces/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  componentes: Observable<Componente[]>;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataMenu: DataService,
   ) {
     this.initializeApp();
   }
+
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  ngOnInit(): void {
+      this.componentes = this.dataMenu.getMenuOpts();
   }
 }
