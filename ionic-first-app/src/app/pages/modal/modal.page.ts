91658f89f082f5a6be998a89b10ed753649078b4
Anderson Revelo <fabiander_21@hotmail.es>
2021-04-25 17:07:32 -0500

    modal les y comunicacion padre e hijo 2

---
+++ ionic-first-app/src/app/pages/modal/modal.page.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import {ModalInfoPage} from '../modal-info/modal-info.page';
+
+@Component({
+    selector: 'app-modal',
+    templateUrl: './modal.page.html',
+    styleUrls: ['./modal.page.scss'],
+})
+export class ModalPage implements OnInit {
+
+    constructor(private modalCtrl: ModalController) {
+    }
+
+    ngOnInit() {
+    }
+
+    async mostrarModal() {
+        const modal = await this.modalCtrl.create(
+            {
+                component: ModalInfoPage,
+                componentProps: {
+                    nombre: 'Anderson',
+                    pais: 'Ecuador',
+                }
+            }
+        );
+        await modal.present();
+
+        // para recibir los que manda el hijo
+        const {data} = await modal.onDidDismiss();
+        console.log(data);
+
+        // diferencia entre ondisdismois y onwilldismis
+        // con willdismiss los datos son enviados hasta antes de la animacion de cierre del modal
+    }
+}
