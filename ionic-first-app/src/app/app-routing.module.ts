77be69db95fe294c6e568c08dad2005e35b49442
Anderson Revelo <fabiander_21@hotmail.es>
2021-04-25 17:05:06 -0500

    modal les y comunicacion padre e hijo

--- ionic-first-app/src/app/app-routing.module.ts
+++ ionic-first-app/src/app/app-routing.module.ts
@@ -1,63 +1,69 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full'
+    {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
 
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'alert',
-    loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
-  },
-  {
-    path: 'action-sheet',
-    loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
-  },
-  {
-    path: 'avatar',
-    loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
-  },
-  {
-    path: 'buttons',
-    loadChildren: () => import('./pages/buttons/buttons.module').then( m => m.ButtonsPageModule)
-  },
-  {
-    path: 'check',
-    loadChildren: () => import('./pages/check/check.module').then( m => m.CheckPageModule)
-  },
-  {
-    path: 'date-time',
-    loadChildren: () => import('./pages/date-time/date-time.module').then( m => m.DateTimePageModule)
-  },
-  {
-    path: 'fab',
-    loadChildren: () => import('./pages/fab/fab.module').then( m => m.FabPageModule)
-  },
-  {
-    path: 'grid',
-    loadChildren: () => import('./pages/grid/grid.module').then( m => m.GridPageModule)
-  },
-  {
-    path: 'infinite',
-    loadChildren: () => import('./pages/infinite/infinite.module').then( m => m.InfinitePageModule)
-  },
-  {
-    path: 'list-reorder',
-    loadChildren: () => import('./pages/list-reorder/list-reorder.module').then( m => m.ListReorderPageModule)
-  },
-  
+    },
+    {
+        path: 'inicio',
+        loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
+    },
+    {
+        path: 'alert',
+        loadChildren: () => import('./pages/alert/alert.module').then(m => m.AlertPageModule)
+    },
+    {
+        path: 'action-sheet',
+        loadChildren: () => import('./pages/action-sheet/action-sheet.module').then(m => m.ActionSheetPageModule)
+    },
+    {
+        path: 'avatar',
+        loadChildren: () => import('./pages/avatar/avatar.module').then(m => m.AvatarPageModule)
+    },
+    {
+        path: 'buttons',
+        loadChildren: () => import('./pages/buttons/buttons.module').then(m => m.ButtonsPageModule)
+    },
+    {
+        path: 'check',
+        loadChildren: () => import('./pages/check/check.module').then(m => m.CheckPageModule)
+    },
+    {
+        path: 'date-time',
+        loadChildren: () => import('./pages/date-time/date-time.module').then(m => m.DateTimePageModule)
+    },
+    {
+        path: 'fab',
+        loadChildren: () => import('./pages/fab/fab.module').then(m => m.FabPageModule)
+    },
+    {
+        path: 'grid',
+        loadChildren: () => import('./pages/grid/grid.module').then(m => m.GridPageModule)
+    },
+    {
+        path: 'infinite',
+        loadChildren: () => import('./pages/infinite/infinite.module').then(m => m.InfinitePageModule)
+    },
+    {
+        path: 'list-reorder',
+        loadChildren: () => import('./pages/list-reorder/list-reorder.module').then(m => m.ListReorderPageModule)
+    },
+    {
+        path: 'modal',
+        loadChildren: () => import('./pages/modal/modal.module').then(m => m.ModalPageModule)
+    },
+
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
