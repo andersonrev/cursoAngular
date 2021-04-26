fb97065260930be3633bf53ccdabaf39f3617385
Anderson Revelo <fabiander_21@hotmail.es>
2021-04-25 22:56:18 -0500

    search sin busqueda

---
+++ ionic-first-app/src/app/pages/search/search.page.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../services/data.service';
+
+@Component({
+    selector: 'app-search',
+    templateUrl: './search.page.html',
+    styleUrls: ['./search.page.scss'],
+})
+export class SearchPage implements OnInit {
+
+    albumnes: any[ ] = [];
+
+    constructor(private dataService: DataService) {
+    }
+
+    ngOnInit() {
+        this.dataService.getAlbums().subscribe(albumnes => {
+            this.albumnes = albumnes;
+        });
+    }
+
+    onSearchChange(event: any) {
+        console.log(event);
+    }
+}
--- ionic-first-app/src/app/services/data.service.ts
+++ ionic-first-app/src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Componente} from '../interfaces/interfaces';
 import {Injectable} from '@angular/core';
+
 @Injectable({
     providedIn: 'root'
 })
@@ -11,6 +12,10 @@ export class DataService {
 
     getUsuarios() {
         return this.http.get('http://jsonplaceholder.typicode.com/users');
+    }
+
+    getAlbums() {
+        return this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums');
     }
 
     getMenuOpts() {
