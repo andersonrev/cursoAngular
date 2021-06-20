2eea8fd0efddac4d775374a407e0bba658679ce6
andersonrev <fabiander_21@hotmail.es>
2021-06-20 14:48:46 -0500

    componentes modulares de noticias

--- noticias/src/app/pages/tab1/tab1.page.ts
+++ noticias/src/app/pages/tab1/tab1.page.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticasService } from '../../services/noticas.service';
+import { Article, RespuestoTopHeadlines } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
+
+  noticias: Article[] = [];
 
   constructor(private noticiasServices: NoticasService) { }
-   ngOnInit(){
-     this.noticiasServices.getTopHeadLines().subscribe(val => console.log('noticias',val));
-   }
+  ngOnInit() {
+    this.noticiasServices.getTopHeadLines().subscribe((resp) => {
+      console.log('noticias', resp)
+      //this.noticias = resp.articles;
+      this.noticias.push(...resp.articles);
+    });
+
+  }
 
 }
--- noticias/src/app/services/noticas.service.ts
+++ noticias/src/app/services/noticas.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RespuestoTopHeadlines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class NoticasService {
 
   constructor(private http: HttpClient) { }
   getTopHeadLines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?apiKey=97b9603fcb744b37ae5adc74cf1d32cd&country=us`);
+    return this.http.get<RespuestoTopHeadlines>(`https://newsapi.org/v2/top-headlines?apiKey=97b9603fcb744b37ae5adc74cf1d32cd&country=us`);
 
   }
 }
