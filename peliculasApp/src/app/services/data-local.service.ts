fa75e3378f49b28d0055766dd3c991c72f21649d
andersonrev <fabiander_21@hotmail.es>
2021-07-22 00:48:26 -0500

    feat: 157

--- peliculasApp/src/app/services/data-local.service.ts
+++ peliculasApp/src/app/services/data-local.service.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@angular/core';
 import { PeliculaDetalle } from '../interfaces/interfaces';
-import { Storage} from '@ionic/storage';
+import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataLocalService {
+
   peliculas: PeliculaDetalle[] = [];
 
-  constructor(private storage: Storage) {
+  constructor(private storage: Storage,
+    private toastCtrl: ToastController) {
     this.storage.create();
-   }
-  guardarPelicula(pelicula: PeliculaDetalle){
-    this.peliculas.push(pelicula);
-    this.storage.set('peliculas',this.peliculas);
+  }
+
+  async presentToast(message: string){
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1000
+    });
+    toast.present();
+  }
+  guardarPelicula(pelicula: PeliculaDetalle) {
+    let existe = false;
+    let mensaje = '';
+
+    for (const peli of this.peliculas) {
+      if (peli.id === pelicula.id) {
+        existe = true;
+        break;
+      }
+    }
+
+    if (existe) {
+      this.peliculas = this.peliculas.filter(peli => peli.id !== pelicula.id);
+      mensaje = 'Removido de favoritos';
+    }else {
+      this.peliculas.push(pelicula);
+      mensaje = 'Agregadda a favoritos';
+    }
+    this.presentToast(mensaje);
+    this.storage.set('peliculas', this.peliculas);
   }
 }
