61370d8c536df428a1d839a9322f755d5802572c
andersonrev <fabiander_21@hotmail.es>
2021-07-26 09:27:37 -0500

    feat: storage ojo pilas aqui

--- qrscaneer/src/app/services/data-local.service.ts
+++ qrscaneer/src/app/services/data-local.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Registro } from '../models/registro.model';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataLocalService {
   guardados: Registro[] = [];
-  constructor() { }
-  guardarRegistro(format: string, text: string){
-      const nuevoRegistro = new Registro(format, text);
-      this.guardados.unshift(nuevoRegistro);
-      console.log(this.guardados);
+
+  constructor(private storage: Storage) {
+    this.storage.create();
+   
+    this.cargarStorage();
+
+   }
+
+   async cargarStorage(){
+     this.guardados = (await this.storage.get('registros')) || [];
+   }
+
+  guardarRegistro(format: string, text: string) {
+    const nuevoRegistro = new Registro(format, text);
+    this.guardados.unshift(nuevoRegistro);
+    console.log(this.guardados);
+    this.storage.set('registros', this.guardados);
   }
 }
