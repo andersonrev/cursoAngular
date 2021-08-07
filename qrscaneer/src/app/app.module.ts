26e37ff019b3b7664dffc78e277adbcb3521ec18
andersonrev <fabiander_21@hotmail.es>
2021-08-07 16:15:24 -0500

    no salio lo del email

--- qrscaneer/src/app/app.module.ts
+++ qrscaneer/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
+import { File } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -22,7 +23,9 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     BarcodeScanner,
-    InAppBrowser],
+    InAppBrowser,
+    File,
+    EmailComposer],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- qrscaneer/src/app/services/data-local.service.ts
+++ qrscaneer/src/app/services/data-local.service.ts
@@ -3,6 +3,8 @@ import { Registro } from '../models/registro.model';
 import { Storage } from '@ionic/storage';
 import { NavController } from '@ionic/angular';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,9 @@ export class DataLocalService {
 
   constructor(private storage: Storage,
     private navCtrl: NavController,
-    private inAppBrowser: InAppBrowser) {
+    private inAppBrowser: InAppBrowser,
+    private file: File,
+    private emailCompose: EmailComposer) {
     this.storage.create();
 
     this.cargarStorage();
@@ -62,7 +66,45 @@ export class DataLocalService {
 
     });
 
-    console.log(arregloTemp.join(''));
+    this.crearArchivoFisico(arregloTemp.join(''));
 
+  }
+
+  crearArchivoFisico(text: string) {
+    this.file.checkFile(this.file.dataDirectory, 'registros.csv').then(
+      existe => {
+        console.log('Existe', existe);
+        return this.escribirEnArchivo(text);
+      }
+    ).catch(err => {
+      return this.file.createFile( this.file.dataDirectory, 'registros.csv', false).then(
+        creado => this.escribirEnArchivo(text)
+      ).catch(
+        err => {
+          console.error('No se pudo crear el archivo', err);
+        }
+      )
+    });
+
+  }
+
+  async escribirEnArchivo(text: string) {
+      await this.file.writeExistingFile(this.file.dataDirectory, 'registros.csv', text);
+      console.log('archivo creado');
+      const archivo = `${this.file.dataDirectory}registros.csv`
+      // console.log(this.file.dataDirectory + 'registros.csv');
+      const email = {
+        to: 'fabiander94@gmail.com',
+        // cc: 'erika@mustermann.de',
+        // bcc: ['john@doe.com', 'jane@doe.com'],
+        attachments: [
+         archivo
+        ],
+        subject: 'Backup',
+        body: 'Aqui tiene  los backups pros',
+        isHtml: true
+      };
+
+      this.emailCompose.open(email);
   }
 }
