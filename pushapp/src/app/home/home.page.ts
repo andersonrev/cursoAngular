9b8a5440f592f3247933be36f6a7c9014d5296a1
andersonrev <fabiander_21@hotmail.es>
2021-08-15 09:59:51 -0500

    feat: poryet de push dar click enla notificacion
    NO sale lo de one signal

---
+++ pushapp/src/app/home/home.page.ts
@@ -0,0 +1,28 @@
+import { ApplicationRef, Component, OnInit } from '@angular/core';
+import { PushService } from '../services/push.service';
+import { OSNotificationPayload } from '@ionic-native/onesignal/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  mensajes: OSNotificationPayload[] = [];
+  constructor(private pushService: PushService,
+    private applicationRef: ApplicationRef) { }
+
+
+  ngOnInit() {
+    this.pushService.pushListener.subscribe(noti => {
+      this.mensajes.unshift(noti);
+      this.applicationRef.tick(); // hacer la deteccion de cambios nuevamente
+    })
+  }
+
+  async ionViewWillEnter() {
+    this.mensajes = await this.pushService.getMensajes();
+  }
+
+}
