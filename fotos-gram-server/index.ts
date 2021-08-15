58cdc70992d5ad5dd1a0af605a3f65e205500936
andersonrev <fabiander_21@hotmail.es>
2021-08-15 17:23:18 -0500

    feat: instalando dependencia para server

--- fotos-gram-server/index.ts
+++ fotos-gram-server/index.ts
@@ -1,1 +1,8 @@
-console.log('Nola shu')
+import Server from "./clases/server";
+
+const server = new Server();
+
+// Levantar express
+server.start(()=>{
+    console.log(`Servidor corriendo en puerto ${server.port}`)
+})
