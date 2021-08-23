188d78da6c1337a2d9993ae2ae8a16bb53935723
andersonrev <fabiander_21@hotmail.es>
2021-08-22 22:53:39 -0500

    245- login diseño

--- fotosgram/src/app/pages/login/login.page.ts
+++ fotosgram/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false
+    },
+  ];
+
+  avatarSlide = {
+    slidesPerView: 3.5
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(flogin: NgForm){
+    console.log(flogin.valid);
+  }
+
+  registro(fRegistro: NgForm) {
+    console.log(fRegistro.valid);
+  }
+
+  seleccionarAvatar(avatar){
+
+    // ojo este método tiene como parámetro por referencia el objeto del arreglo, es decir, que se peude cambiar directamente el objeto.
+    this.avatars.forEach(avtr => avtr.seleccionado = false);
+    avatar.seleccionado = true;
+
   }
 
 }
