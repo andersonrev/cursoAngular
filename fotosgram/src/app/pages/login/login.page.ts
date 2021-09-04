c5534975e6c1d10f47cd804d19126b86ba5e872c
andersonrev <fabiander_21@hotmail.es>
2021-09-04 11:14:39 -0500

    250 almacenar token

--- fotosgram/src/app/pages/login/login.page.ts
+++ fotosgram/src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { IonSlides, NavController } from '@ionic/angular';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +10,7 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
 
+  @ViewChild('slidePrincipal') slides: IonSlides;
 
   avatars = [
     {
@@ -44,28 +47,69 @@ export class LoginPage implements OnInit {
     },
   ];
 
+  userLogueado = {
+    email: 'anderson@gmail.com',
+    password: '1234'
+  }
+
   avatarSlide = {
     slidesPerView: 3.5
   }
 
-  constructor() { }
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+
+  constructor(private usuarioService: UsuarioService,
+    private navCtrl: NavController) { }
 
   ngOnInit() {
+    // this.slides.lockSwipeToNext(true);
   }
 
-  login(flogin: NgForm){
-    console.log(flogin.valid);
+   ionViewWillEnter(){
+
+    this.slides.lockSwipes(true);
+   }
+  async login(flogin: NgForm) {
+    if(flogin.invalid){
+      return;
+    }
+
+    const valido = await this.usuarioService.login(this.userLogueado.email, this.userLogueado.password);
+    if (valido){
+          this.navCtrl.navigateRoot('main/tabs/tab1', { animated: true});
+    }else {
+
+    }
+
   }
 
   registro(fRegistro: NgForm) {
     console.log(fRegistro.valid);
   }
 
-  seleccionarAvatar(avatar){
+  seleccionarAvatar(avatar) {
 
     // ojo este método tiene como parámetro por referencia el objeto del arreglo, es decir, que se peude cambiar directamente el objeto.
     this.avatars.forEach(avtr => avtr.seleccionado = false);
     avatar.seleccionado = true;
+
+  }
+
+  mostrarRegistro() {
+
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(0);
+    this.slides.lockSwipes(true);
+
+  }
+
+  mostrarLogin() {
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(1);
+    this.slides.lockSwipes(true);
 
   }
 
---
+++ fotosgram/src/app/services/usuario.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
+import { environment } from '../../environments/environment';
+
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  token: string = null;
+
+
+  constructor(private http: HttpClient,
+    private storage: Storage) {
+
+    this.storage.create();
+
+  }
+
+  login(email: string, password: string): Promise<boolean> {
+    return new Promise(resolve => {
+      const user = { email, password };
+      this.http.post(`${URL}/user/login`, user).subscribe(
+        res => {
+          console.log(res);
+
+          if (res['ok']) {
+            this.guardarToken(res['token']);
+            resolve(true);
+          } else {
+            this.token = null;
+            this.storage.clear();
+            resolve(false);
+          }
+        }
+      )
+    });
+
+
+  }
+
+  async guardarToken(token: string) {
+    this.token = token;
+    await this.storage.set('token', token);
+
+  }
+
+}
