c0c86456fc28c7757ddff32990e6d205265e3e3d
andersonrev <fabiander_21@hotmail.es>
2021-10-03 00:09:00 -0500

    instancia local de sevicio de loading

--- rxjs/reactive-angular-course-1-start/src/app/course-dialog/course-dialog.component.ts
+++ rxjs/reactive-angular-course-1-start/src/app/course-dialog/course-dialog.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {Course} from '../model/course';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import * as moment from 'moment';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
@@ -9,52 +9,54 @@ import {CoursesService} from '../services/courses.service';
 import {LoadingService} from '../loading/loading.service';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css']
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css'],
+  providers: [LoadingService]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
-    form: FormGroup;
+  form: FormGroup;
 
-    course:Course;
+  course: Course;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService,
-        private loadingService: LoadingService) {
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private coursesService: CoursesService,
+    private loadingService: LoadingService) {
 
-        this.course = course;
+    this.course = course;
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription, Validators.required]
-        });
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required]
+    });
 
-    }
+  }
 
-    ngAfterViewInit() {
+  ngAfterViewInit() {
 
-    }
+  }
 
-    save() {
+  save() {
 
-      const changes = this.form.value;
-      this.coursesService.saveCourse(this.course.id, changes)
-        .subscribe(
-          val => {
-            this.dialogRef.close(val);
-          }
-        );
+    const changes = this.form.value;
+    const courseSave$ = this.coursesService.saveCourse(this.course.id, changes);
+    this.loadingService.showLoaderUntilCompleted(courseSave$)
+      .subscribe(
+        val => {
+          this.dialogRef.close(val);
+        }
+      );
 
-    }
+  }
 
-    close() {
-        this.dialogRef.close();
-    }
+  close() {
+    this.dialogRef.close();
+  }
 
 }
