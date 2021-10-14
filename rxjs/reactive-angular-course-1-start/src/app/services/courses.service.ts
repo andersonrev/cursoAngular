0371377887350ba8bf82fc30eae6c5557158b6f9
andersonrev <fabiander_21@hotmail.es>
2021-10-14 08:52:55 -0500

    finish sect 5, and ng container and ng template

--- rxjs/reactive-angular-course-1-start/src/app/services/courses.service.ts
+++ rxjs/reactive-angular-course-1-start/src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Course} from '../model/course';
 import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
+import {Lesson} from '../model/lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +26,37 @@ export class CoursesService {
       .pipe(
         shareReplay()
       );
+  }
+
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        filter: search,
+        pageSize: '100'
+      }
+    }).pipe(
+      map(res => res['payload']),
+      shareReplay()
+    );
+  }
+
+  loadCoursesById(courseId: number) {
+    return this.http.get<Course>(`/api/courses/${courseId}`)
+      .pipe(
+        shareReplay()
+      );
+
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: '10000',
+        courseId: courseId.toString()
+      }
+    }).pipe(
+      map(res => res['payload']),
+      shareReplay()
+    );
   }
 }
