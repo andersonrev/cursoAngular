07e6c304462b2d6a2b36cec47e178d750a76028a
andersonrev <fabiander_21@hotmail.es>
2022-01-16 17:37:22 -0500

    quitar proyectos ionic

--- peliculasApp/src/app/pipes/filtro-imagen.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FiltroImagenPipe } from './filtro-imagen.pipe';
-
-describe('FiltroImagenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FiltroImagenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
