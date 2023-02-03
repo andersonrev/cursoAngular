e238e4d6ece25ccf52207cbbbe8123e4786600a7
andersonrev <fabiander_21@hotmail.es>
2023-02-02 22:07:45 -0500

    angular-advance 80 take map  operator

--- angular-advance/adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ angular-advance/adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { Observable, interval } from 'rxjs';
+import { retry, take, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -11,9 +11,31 @@ export class RxjsComponent {
 
   constructor(){
 
+
+    // this.returnObservable()
+    //   .pipe(retry(2))
+    //   .subscribe({
+    //   next: valor => console.log('Subs', valor),
+    //   error: err => console.warn('Error', err),
+    //   complete: () => console.info('Termino'),
+    // })
+
+
+    this.returnInterval().subscribe(console.log);
+  }
+
+  returnInterval(): Observable<number>{
+    return interval( 1000 )
+      .pipe(
+        take( 4 ),
+        map( valor => valor + 1 )
+      );
+  }
+
+  returnObservable(): Observable<number>{
     let i = -1;
 
-    const obs$ = new Observable(observer => {
+    return new Observable<number>(observer => {
       const intervalo = setInterval(()=> {
 
         i++;
@@ -25,20 +47,11 @@ export class RxjsComponent {
         }
 
         if( i == 2 ){
-          i = 0;
           observer.error('Errosito');
         }
 
-      },1000);
+      }, 1000 );
 
     });
-
-    obs$
-      .pipe(retry(2))
-      .subscribe({
-      next: valor => console.log('Subs', valor),
-      error: err => console.warn('Error', err),
-      complete: () => console.info('Termino'),
-    })
   }
 }
