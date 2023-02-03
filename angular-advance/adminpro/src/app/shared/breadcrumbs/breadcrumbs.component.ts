f4824ba752c7d3849ae8a7ebf4da42a9c2f597c3
andersonrev <fabiander_21@hotmail.es>
2023-02-03 01:14:30 -0500

    angular-advance migas de pan y fin secction 8

--- angular-advance/adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ angular-advance/adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  public titulo = new BehaviorSubject<string>('');
+
+  public titulo2!: string;
+
+  public tituloSubs$!: Subscription;
+
+  constructor(private router: Router) {
+
+    this.tituloSubs$ = this.getArgumentosRuta2().subscribe(({titulo})=> {
+      this.titulo2 = titulo;
+      document.title = `AdminPro - ${ titulo }`
+    })
+
+  }
 
   ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+
+  }
+
+
+  // getArgumentosRuta(){
+
+  //   this.router.events.pipe(
+  //     filter((event: any) => event instanceof ActivationEnd),
+  //     filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+  //     map((event: ActivationEnd) => event.snapshot.data.titulo),
+  //   ).subscribe(valor => this.titulo.next(valor))
+  // }
+
+
+  getArgumentosRuta2(){
+
+    return this.router.events.pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data),
+    );
   }
 
 }
