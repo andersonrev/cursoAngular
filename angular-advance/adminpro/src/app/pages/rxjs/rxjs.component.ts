ce305ed0a4a5be084b25b570e69cc7c328f567f4
andersonrev <fabiander_21@hotmail.es>
2023-02-02 22:21:25 -0500

    angular-advance 83 unsubscribe

--- angular-advance/adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ angular-advance/adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { retry, take, map, filter } from 'rxjs/operators';
 
 @Component({
@@ -7,7 +7,10 @@ import { retry, take, map, filter } from 'rxjs/operators';
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
+
+
+  subscripciones!: Subscription;
 
   constructor(){
 
@@ -21,13 +24,18 @@ export class RxjsComponent {
     // })
 
 
-    this.returnInterval().subscribe(console.log);
+    this.subscripciones = this.returnInterval().subscribe(console.log);
+  }
+
+  ngOnDestroy(): void {
+
+    this.subscripciones.unsubscribe();
   }
 
   returnInterval(): Observable<number>{
     return interval( 100 )
       .pipe(
-        take( 10 ),
+        // take( 10 ),
         map( valor => valor + 1 ),
         filter( valor => valor%2 === 0 ),
       );
