477e59ea9bee4a6603fda6405e39089e27298d60
andersonrev <fabiander_21@hotmail.es>
2023-02-02 21:45:18 -0500

    angular-advance 77

--- angular-advance/adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ angular-advance/adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class RxjsComponent {
 
+  constructor(){
+
+    let i = -1;
+
+    const obs$ = new Observable(observer => {
+      const intervalo = setInterval(()=> {
+
+        i++;
+        observer.next(i);
+
+        if( i === 4 ){
+          clearInterval(intervalo);
+          observer.complete();
+        }
+
+        if( i == 2 ){
+          observer.error('Errosito');
+        }
+
+      },1000);
+
+    });
+
+    obs$.subscribe({
+      next: valor => console.log('Subs', valor),
+      error: err => console.warn('Error', err),
+      complete: () => console.info('Termino'),
+    })
+  }
 }
