f1a8c72223fd47c84c9b65c820e176ac568548a6
andersonrev <fabiander_21@hotmail.es>
2023-02-19 18:11:30 -0500

    109-angular-advance validacion existe email

--- angular-advance/04-backend-server/controllers/usuarios.js
+++ angular-advance/04-backend-server/controllers/usuarios.js
@@ -1,4 +1,5 @@
 const Usuario = require('../models/usuario')
+const { response } = require('express');
 
 const getUsuarios = async(req,res) => {
 
@@ -11,18 +12,41 @@ const getUsuarios = async(req,res) => {
 }
 
 
-const crearUsuario = async(req,res) => {
+const crearUsuario = async(req,res = response) => {
 
   const { email, password, nombre } = req.body;
 
-  const usuario = new Usuario(req.body);
+  try {
 
-  await usuario.save();
-  
-  res.json({
-    ok: true,
-    usuario
-  });
+    const existeEmail = await Usuario.findOne({email});
+
+    if( existeEmail ){
+      return res.status(400).json({
+        ok: false,
+        mensaje: 'El correo ya esta registrado'
+      })
+    }
+
+    const usuario = new Usuario(req.body);
+    await usuario.save();
+
+    res.json({
+      ok: true,
+      usuario
+    });
+
+  }catch(error){
+
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      mensaje: 'Error inesperado... revisar logs'
+    });
+
+  }
+
+
+
 }
 
 module.exports = {
