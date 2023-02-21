050a6efd2a30dd3b155a0c3000b6e9053e28c80d
andersonrev <fabiander_21@hotmail.es>
2023-02-20 21:24:05 -0500

    118-angular-advance-middleaware token y fin seccion 10

---
+++ angular-advance/04-backend-server/middlewares/validar-jwt.js
@@ -0,0 +1,38 @@
+const { response } = require('express');
+const jwt = require('jsonwebtoken');
+
+const validarJWT = (req, res = response, next) => {
+
+  const token = req.header('x-token');
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      mensaje: 'No hay token en la peticion'
+    });
+  }
+
+  try {
+
+    const { uid } = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.uid = uid;
+    next();
+
+
+
+  } catch (err) {
+    return res.status(401).json({
+      ok: false,
+      mensaje: 'Token no valido'
+    })
+  }
+
+
+
+
+}
+
+module.exports = {
+  validarJWT
+}
--- angular-advance/04-backend-server/routes/usuarios.js
+++ angular-advance/04-backend-server/routes/usuarios.js
@@ -5,15 +5,17 @@ const Router  = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { getUsuarios, crearUsuario, actualizarUsuario, deleteUsuario } = require('../controllers/usuarios')
+const { getUsuarios, crearUsuario, actualizarUsuario, deleteUsuario } = require('../controllers/usuarios');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
 
-router.get('/', getUsuarios );
+router.get('/', validarJWT ,getUsuarios );
 
 router.post('/',
   [
+    validarJWT,
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('password','El password es obligatorio').not().isEmpty(),
     check('email','El email es obligatorio').isEmail(),
@@ -24,6 +26,8 @@ router.post('/',
 
 router.put('/:id', 
   [
+
+    validarJWT,
     check('nombre','El nombre es obligatorio').not().isEmpty(),
     check('email','El email es obligatorio').isEmail(),
     validarCampos,
@@ -31,6 +35,7 @@ router.put('/:id',
   actualizarUsuario);
 
 router.delete('/:id', 
+  validarJWT,
   deleteUsuario
 );
 
