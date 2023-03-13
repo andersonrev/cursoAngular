ce4c88f6c822f22045fc2945e888242765419fa2
andersonrev <fabiander_21@hotmail.es>
2023-03-12 19:39:25 -0500

    129 promise all

--- angular-advance/04-backend-server/controllers/usuarios.js
+++ angular-advance/04-backend-server/controllers/usuarios.js
@@ -5,11 +5,22 @@ const { generarJWT } = require('../helpers/jwt');
 
 const getUsuarios = async (req, res) => {
 
-  const usuarios = await Usuario.find();
+  const desde = Number(req.query.desde) || 0;
+
+  const [usuarios, total] = await Promise.all(
+    [
+      Usuario.find({}, 'nombre email role google')
+        .skip(desde)
+        .limit(5),
+
+      Usuario.count()
+    ]
+  );
+
   res.json({
     ok: true,
     usuarios,
-    uid: req.uid
+    total
   });
 
 }
@@ -136,7 +147,7 @@ const deleteUsuario = async (req, res = response) => {
     res.json(
       {
         ok: true,
-        mensaje:'Usuario eliminado'
+        mensaje: 'Usuario eliminado'
       }
     )
 
